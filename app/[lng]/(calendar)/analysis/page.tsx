a929098b734803039b07e8774943af0a1206e78e
Jehyun Jung <congmul@hotmail.com>
2024-12-31 14:51:38 -0800

    Clean up for Analysis page

--- app/[lng]/(calendar)/analysis/page.tsx
+++ app/[lng]/(calendar)/analysis/page.tsx
@@ -2,13 +2,13 @@ import { Suspense } from 'react';
 import { CompactCalendarHandler } from '@/app/ui/shared-components';
 import { AnalysisPageSkeleton } from '@/app/ui/skeletons';
 
-export default async function Page() {
+export default async function Page({ params: { lng } } : { params: { lng : string }}) {
     return (<>
       <div className="analysis-page-wrapper">
         <CompactCalendarHandler />
-            <Suspense fallback={<div>Skeleton loading..</div>}>
-                Analysis Charts
-            </Suspense>
+        <Suspense fallback={<div>Skeleton loading..</div>}>
+            Analysis Charts
+        </Suspense>
       </div>
     </>
     );
