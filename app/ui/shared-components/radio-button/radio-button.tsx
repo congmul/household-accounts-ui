50140acd40e7284300a1e28a74466d34c8a15d92
Jehyun Jung <congmul@hotmail.com>
2025-08-03 10:18:38 -0700

    Fix issue

--- app/ui/shared-components/radio-button/radio-button.tsx
+++ app/ui/shared-components/radio-button/radio-button.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
 
 interface RadioButtonType {
     label:string,
     name:string, 
     value:string, 
     checked:boolean, 
-    onChange: (e:any) => void
+    onChange: (e:any) => void,
 }
-const RadioButton:React.FC<RadioButtonType> = ({ label, name, value, checked, onChange }) => {
-  return (
-    <label className="flex items-center space-x-2">
+const RadioButton:React.FC<RadioButtonType> = ({ label, name, value, checked, onChange, ...rest }) => {
+  return (<>
+    <label
+      className="flex items-center space-x-2"
+    >
       <input
         type="radio"
         name={name}
         value={value}
-        checked={checked}
         onChange={onChange}
         className="form-radio text-blue-500 h-5 w-5 cursor-pointer"
       />
-      <span className="text-gray-700 cursor-pointer"  style={{ paddingTop: '4px' }}>{label}</span>
+      <span className="text-gray-700 cursor-pointer ml-1"  style={{ paddingTop: '4px' }}>{label}</span>
     </label>
+    </>
   );
 };
 
