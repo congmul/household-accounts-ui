c832313a64f3bbe526393499c14d2d96e85275b5
Jehyun Jung <congmul@hotmail.com>
2025-09-18 09:32:25 -0700

    add a promp

--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -47,7 +47,7 @@ export const Header = ({ lng }: { lng: string }) => {
     <div className="header flex justify-around items-center bg-red-300 text-white h-[70px]">
       {pageName === 'settings' && t(`${pageName}.header.title`)}
     </div>
-    { accountBooks?.length === 0 && defaultAccountBook == null  && <AccountBookForm isOpen={true} close={() => {}} /> }
+    { accountBooks?.length === 0 && defaultAccountBook == null  && <AccountBookForm isOpen={true} isFirstTime={true} close={() => {}} /> }
   </>
   )
 }
